9ed6be56c77c322c7ac7e16c488b4c84f99a44c1
Leon-Gu <guyunx@sheridancollege.ca>
2024-03-14 18:46:18 -0400

    Task 4b done

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,19 +1,42 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
     public static Villager SelectedVillager { get; private set; }
+    public TMP_Text classUI;
+
     public static void SetSelectedVillager(Villager villager)
     {
+
         if(SelectedVillager != null)
         {
             SelectedVillager.Selected(false);
         }
         SelectedVillager = villager;
-        SelectedVillager.Selected(true);
+        
+        SelectedVillager.Selected(true); 
+    }
+
+    private void Update()
+    {
+        classIndicator();
+    }
+
+    void classIndicator()
+    {
+        if (SelectedVillager == null)
+        {
+            classUI.text = "Villager";
+        }
+
+        if (SelectedVillager != null)
+        {
+            classUI.text = SelectedVillager.GetType().Name;
+        }
     }
     
 }
